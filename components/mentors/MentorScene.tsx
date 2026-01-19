9cc3f86893f2cea9a14117258f7752b140088f8d
irfan gyan <irfangyan325@gmail.com>
2026-01-19 01:20:21 -0800

    updated code

--- components/mentors/MentorScene.tsx
+++ components/mentors/MentorScene.tsx
@@ -295,7 +295,7 @@ export function MentorScene({
 
   return (
     <div className="relative mx-auto mt-10 w-full overflow-hidden">
-      <div className="relative h-[620px] w-full overflow-hidden border border-imperium-gold/25 bg-black shadow-[0_0_70px_rgba(0,0,0,0.9)]">
+      <div className="relative h-[720px] w-full overflow-hidden border border-imperium-gold/25 bg-black shadow-[0_0_70px_rgba(0,0,0,0.9)]">
 
         <div className="absolute inset-0 z-0 grid grid-cols-3 brightness-100">
           {[m0, m1, m2].map((m, idx) => (
